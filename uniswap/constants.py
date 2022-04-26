2482c5cbffe048de5707e2a622c7d67a730deeeb
healthyred <jhl3@williams.edu>
2022-04-26 14:04:29 -0400

    adding more abis + constants

---
+++ uniswap/constants.py
@@ -0,0 +1,63 @@
+ETH_ADDRESS = "0x0000000000000000000000000000000000000000"
+WETH9_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+
+# see: https://chainid.network/chains/
+_netid_to_name = {
+    1: "mainnet",
+    3: "ropsten",
+    4: "rinkeby",
+    10: "optimism",
+    42: "kovan",
+    56: "binance",
+    97: "binance_testnet",
+    137: "polygon",
+    100: "xdai",
+    250: "fantom",
+    42161: "arbitrum",
+    421611: "arbitrum_testnet",
+    1666600000: "harmony_mainnet",
+    1666700000: "harmony_testnet",
+}
+
+_factory_contract_addresses_v1 = {
+    "mainnet": "0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95",
+    "ropsten": "0x9c83dCE8CA20E9aAF9D3efc003b2ea62aBC08351",
+    "rinkeby": "0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36",
+    "kovan": "0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30",
+    "görli": "0x6Ce570d02D73d4c384b46135E87f8C592A8c86dA",
+}
+
+
+# For v2 the address is the same on mainnet, Ropsten, Rinkeby, Görli, and Kovan
+# https://uniswap.org/docs/v2/smart-contracts/factory
+_factory_contract_addresses_v2 = {
+    "mainnet": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    "ropsten": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    "rinkeby": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    "görli": "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
+    "xdai": "0xA818b4F111Ccac7AA31D0BCc0806d64F2E0737D7",
+    "binance": "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
+    "binance_testnet": "0x6725F303b657a9451d8BA641348b6761A6CC7a17",
+    # SushiSwap on Harmony
+    "harmony_mainnet": "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+    "harmony_testnet": "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+}
+
+_router_contract_addresses_v2 = {
+    "mainnet": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    "ropsten": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    "rinkeby": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    "görli": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    "xdai": "0x1C232F01118CB8B424793ae03F870aa7D0ac7f77",
+    "binance": "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+    "binance_testnet": "0xD99D1c33F9fC3444f8101754aBC46c52416550D1",
+    # SushiSwap on Harmony
+    "harmony_mainnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+    "harmony_testnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+}
+
+_non_fungible_manager_addresses = {
+    "mainnet": "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
+    "ropsten": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+
+}
--- uniswap/uniswap.py
+++ uniswap/uniswap.py
@@ -1,6 +1,33 @@
+import os
+import time
+import logging
+import functools
+from typing import List, Any, Optional, Union, Tuple, Dict, Iterable
+
 from web3 import Web3
+from .types import AddressLike
+
+from .util import (
+    _str_to_addr,
+    _addr_to_str,
+    _validate_address,
+    _load_contract,
+    _load_contract_erc20,
+    is_same_address,
+)
+
+from .constants import (
+    _netid_to_name,
+    _factory_contract_addresses_v1,
+    _factory_contract_addresses_v2,
+    _router_contract_addresses_v2,
+    ETH_ADDRESS,
+)
 
 PROVIDER='https://mainnet.infura.io/v3/49d9273a4f5c446697ee32b9af8bc7cc'
+
+
+logger = logging.getLogger(__name__)
 
 
 class UniswapV3:
@@ -10,7 +37,68 @@ class UniswapV3:
 
     w3: Web3
 
-    def __init__(self, web3: Web3 = None) -> None:
+    def __init__(
+        self,
+        address: Union[AddressLike, str, None],
+        private_key: Optional[str],
+        provider: str = None,
+        use_estimate_gas: bool = True,
+        network: str = 'ETHEREUM',
+        web3: Web3 = None,
+        non_fungible_manager_contract_addr: str = None
+    ) -> None:
+        """
+        :param address: The public address of the ETH wallet to use.
+        :param private_key: The private key of the ETH wallet to use.
+        :param provider: Can be optionally set to a Web3 provider URI. If none set, will fall back to the PROVIDER environment variable, or web3 if set.
+        :param web3: Can be optionally set to a custom Web3 instance.
+        :param network: Can be optionally set to other chains.
+        :param non_fungible_manager_contract_addr: Can be optionally set to override the address of the factory contract.
+        """
+        self.address = _str_to_addr(
+            address or "0x0000000000000000000000000000000000000000"
+        )
+        self.private_key = (
+            private_key
+            or "0x0000000000000000000000000000000000000000000000000000000000000000"
+        )
+
         if web3:
             self.w3 = web3
+        else:
+            # Initialize web3. Extra provider for testing.
+            if not provider:
+                provider = os.environ["PROVIDER"]
+            self.w3 = Web3(Web3.HTTPProvider(PROVIDER, request_kwargs={"timeout": 60}))
 
+        # Cache netid to avoid extra RPC calls
+        self.netid = int(self.w3.net.version)
+        if self.netid in _netid_to_name:
+            self.netname = _netid_to_name[self.netid]
+        else:
+            raise Exception(f"Unknown netid: {self.netid}")
+        logger.info(f"Using {self.w3} ('{self.netname}', netid: {self.netid})")
+
+        # https://github.com/Uniswap/uniswap-v3-periphery/blob/main/deploys.md
+        factory_contract_address = _str_to_addr(
+            "0x1F98431c8aD98523631AE4a59f267346ea31F984"
+        )
+        self.factory_contract = _load_contract(
+            self.w3, abi_name="uniswap-v3/factory", address=factory_contract_address
+        )
+        quoter_addr = _str_to_addr("0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6")
+        self.router_address = _str_to_addr(
+            "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+        )
+        self.quoter = _load_contract(
+            self.w3, abi_name="uniswap-v3/quoter", address=quoter_addr
+        )
+        self.router = _load_contract(
+            self.w3, abi_name="uniswap-v3/router", address=self.router_address
+        )
+        non_fungible_manager_contract_addr = _str_to_addr(
+            "0xC36442b4a4522E871399CD717aBDD847Ab11FE88"
+        )
+        self.non_fungible_manager = _load_contract(
+            self.w3, abi_name="uniswap-v3/nonfungiblemanager", address=non_fungible_manager_contract_addr
+        )
