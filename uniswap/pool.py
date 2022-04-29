9c7dc1fe7c876b09b97a0d6546e2c0181b9259b9
healthyred <jhl3@williams.edu>
2022-04-29 19:24:56 -0400

    adding pool states

--- uniswap/pool.py
+++ uniswap/pool.py
@@ -65,38 +65,61 @@ class State:
         #TODO: 
         pass
 
-w3 = Web3(Web3.HTTPProvider(PROVIDER, request_kwargs={"timeout": 60}))
-poolAddress = _str_to_addr('0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8')
-pool_contract = _load_contract(w3, abi_name="uniswap-v3/pool", address=poolAddress)
 
-def get_pool_immutables():
-    """
-    We write this function as an async func so that we don't have sequential calls
-    to the blockchain where our data becomes out of sync by some number of blocks.
-    TODO: turn this function async.
-    """
-    # test = await asyncio.gather(
-    #     pool_contract.functions.factory().call(),
-    #     pool_contract.functions.token0().call(),
-    #     pool_contract.functions.token1().call(),
-    #     pool_contract.functions.fee().call(),
-    #     pool_contract.functions.tickSpacing().call(),
-    #     pool_contract.functions.maxLiquidityPerTick().call()
-    # )
+class Pool:
 
-    # print(test)
-    factory = pool_contract.functions.factory().call()
-    token0 = pool_contract.functions.token0().call()
-    token1 = pool_contract.functions.token1().call()
-    fee = pool_contract.functions.fee().call()
-    tickSpacing = pool_contract.functions.tickSpacing().call()
-    maxLiquidityPerTick = pool_contract.functions.maxLiquidityPerTick().call()
+    def __init__(
+        self,
+        pool_address_str
+    ):
+        self.w3 = Web3(Web3.HTTPProvider(PROVIDER, request_kwargs={"timeout": 60}))
+        self.pool_address = _str_to_addr(pool_address_str)
+        self.pool_contract = _load_contract(self.w3, abi_name="uniswap-v3/pools", address=self.pool_address)
+        # w3 = Web3(Web3.HTTPProvider(PROVIDER, request_kwargs={"timeout": 60}))
+        # poolAddress = _str_to_addr('0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8')
+        # pool_contract = _load_contract(w3, abi_name="uniswap-v3/pool", address=poolAddress)
 
-    return Immutables(factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick)
+    def get_pool_immutables(self):
+        """
+        We write this function as an async func so that we don't have sequential calls
+        to the blockchain where our data becomes out of sync by some number of blocks.
+        TODO: turn this function async.
+        """
+        factory = self.pool_contract.functions.factory().call()
+        token0 = self.pool_contract.functions.token0().call()
+        token1 = self.pool_contract.functions.token1().call()
+        fee = self.pool_contract.functions.fee().call()
+        tickSpacing = self.pool_contract.functions.tickSpacing().call()
+        maxLiquidityPerTick = self.pool_contract.functions.maxLiquidityPerTick().call()
+
+        return Immutables(factory, token0, token1, fee, tickSpacing, maxLiquidityPerTick)
+
+
+    def get_pool_state(self):
+        """
+        Grabs the state of a given pool.
+        """
+        liquidity = self.pool_contract.functions.liquidity().call()
+        slot = self.pool_contract.functions.slot0().call()
+        return State(
+            liquidity, 
+            sqrt_price_x96=slot[0],
+            tick=slot[1],
+            observation_index=slot[2],
+            observation_cardinality=slot[3],
+            observation_cardinality_next=slot[4],
+            fee_protocol=slot[5],
+            unlocked=slot[6]
+            )
+
 
 if __name__ == "__main__":
-    test = get_pool_immutables()
+    # test = get_pool_immutables()
+    pool = Pool('0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8')
+    test = pool.get_pool_immutables()
 
     print(f'{test.factory}, {test.token0}, {test.token1}, {test.tick_spacing}, {test.fee}, {test.max_liquidity_per_tick}')
 
+    state = pool.get_pool_state()
+    print(f'{state.liquidity}, {state.sqrt_price_x96}, {state.tick}, {state.observation_index}, {state.observation_cardinality}, {state.observation_cardinality_next}, {state.unlocked}')
 
